be7ec68aaa4df3ea52de3e5d75b8edadfa111030
TheTime50 <1004763777@qq.com>
2021-02-20 00:12:39 +0800

    04-webpack体验

--- 02-experience/src/index.js
+++ 02-experience/src/index.js
@@ -1,9 +1,18 @@
-// 03-webpack体验
+// 04-webpack体验
+
+import data from './data.json'
+// import './index.css' // 不能引入css
+
+console.log(data)
 
 function add(x, y) {
     return x + y
 }
 
+console.log(add(4, 5))
 
-
+Array.from({ length: 3 }, (v, i, a) => {
+    console.log(add(i, i))
+    return i
+})
 
